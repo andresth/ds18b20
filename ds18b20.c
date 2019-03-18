e7715bc2669a0343fddd5d199d83ddf4dd6261e7
Thomas Andres <thomas@andres.in>
2019-03-18 23:35:07 +0100

    - support for multiple bus systems - set measurement resolution

--- ds18b20.c
+++ ds18b20.c
@@ -17,24 +17,23 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 #include "rom/ets_sys.h"
+#include "ds18b20.h"
 
-int DS_GPIO;
-int init=0;
 
-/// Sends one bit to bus
-void ds18b20_send(char bit){
-    gpio_set_direction(DS_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_level(DS_GPIO, 0);
+// Sends one bit to bus
+void ds18b20_send(gpio_num_t gpio_num, char bit){
+    gpio_set_direction(gpio_num, GPIO_MODE_OUTPUT);
+    gpio_set_level(gpio_num, 0);
 
     // 6.5µs low for binary 1, 61.5µs low for a binary 0
     // measured with signal analyzer
     if(bit == 1) {
         ets_delay_us(6);
-        gpio_set_level(DS_GPIO, 1);
+        gpio_set_level(gpio_num, 1);
         ets_delay_us(53);
     } else {
         ets_delay_us(61);
-        gpio_set_level(DS_GPIO, 1);
+        gpio_set_level(gpio_num, 1);
     }
 
     // end of slot, 1µs recovery
@@ -42,16 +41,16 @@ void ds18b20_send(char bit){
 }
 
 // Reads one bit from bus
-char ds18b20_read() {
+char ds18b20_read(gpio_num_t gpio_num) {
     // issue read time slot
-    gpio_set_direction(DS_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_level(DS_GPIO, 0);
+    gpio_set_direction(gpio_num, GPIO_MODE_OUTPUT);
+    gpio_set_level(gpio_num, 0);
     ets_delay_us(2);
-    gpio_set_direction(DS_GPIO, GPIO_MODE_INPUT);
+    gpio_set_direction(gpio_num, GPIO_MODE_INPUT);
 
     // read data from bus
     unsigned char duration = 0;
-    while (gpio_get_level(DS_GPIO) == 0) {
+    while (gpio_get_level(gpio_num) == 0) {
         ets_delay_us(1);
         duration++;
     }
@@ -69,20 +68,20 @@ char ds18b20_read() {
 }
 
 // Sends one byte to bus
-void ds18b20_send_byte(char data){
+void ds18b20_send_byte(gpio_num_t gpio_num, char data){
     unsigned char x;
     for(char i=0; i < 8; i++) {
         x = data >> i;
         x &= 0x01;
-        ds18b20_send(x);
+        ds18b20_send(gpio_num, x);
     }
 }
 
 // Reads one byte from bus
-unsigned char ds18b20_read_byte(){
+unsigned char ds18b20_read_byte(gpio_num_t gpio_num){
     unsigned char data = 0;
     for (char i = 0; i < 8; i++) {
-        if(ds18b20_read()) {
+        if(ds18b20_read(gpio_num)) {
             data |= 0x01 << i;
         }
     }
@@ -90,15 +89,15 @@ unsigned char ds18b20_read_byte(){
 }
 
 // Sends reset pulse
-unsigned char ds18b20_RST_PULSE(void){
+unsigned char ds18b20_RST_PULSE(gpio_num_t gpio_num){
     unsigned char duration = 0;
     uint16_t slot_duration = 0;
-    gpio_set_direction(DS_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_level(DS_GPIO,0);
+    gpio_set_direction(gpio_num, GPIO_MODE_OUTPUT);
+    gpio_set_level(gpio_num, 0);
     ets_delay_us(480);
-    gpio_set_direction(DS_GPIO, GPIO_MODE_INPUT);
+    gpio_set_direction(gpio_num, GPIO_MODE_INPUT);
     while (slot_duration <= 500) {  // longer sampling on presence results in a quicker response
-        if(gpio_get_level(DS_GPIO) == 0) {
+        if(gpio_get_level(gpio_num) == 0) {
             duration++;
         }
         ets_delay_us(1);
@@ -113,35 +112,58 @@ unsigned char ds18b20_RST_PULSE(void){
 }
 
 // Returns temperature from sensor
-float ds18b20_get_temp(void) {
-  if(init==1){
-    unsigned char check;
-    char temp1=0, temp2=0;
-      check=ds18b20_RST_PULSE();
-      if(check==1)
-      {
-        ds18b20_send_byte(0xCC);
-        ds18b20_send_byte(0x44);
+float ds18b20_get_temp(DS18B20_Info_t *info) {
+    if(info->init) {
+        unsigned char check;
+        char temp1=0, temp2=0;
+        check = ds18b20_RST_PULSE(info->gpio_num);
+        if(check == 1) {
+            ds18b20_send_byte(info->gpio_num, SKIP_ROM);
+            ds18b20_send_byte(info->gpio_num, CONVERT_T);
 
-        while (ds18b20_read() <= 0) {}
-        check=ds18b20_RST_PULSE();
-        ds18b20_send_byte(0xCC);
-        ds18b20_send_byte(0xBE);
-        temp1=ds18b20_read_byte();
-        temp2=ds18b20_read_byte();
-        float temp=0;
-        temp=(float)(temp1+(temp2*256))/16;
-        return temp;
-      }
-      else{return 0;}
-
-  }
-  else{return 0;}
+            while (ds18b20_read(info->gpio_num) <= 0) {}
+            check = ds18b20_RST_PULSE(info->gpio_num);
+            ds18b20_send_byte(info->gpio_num, SKIP_ROM);
+            ds18b20_send_byte(info->gpio_num, READ_SCRATCHPAD);
+            temp1 = ds18b20_read_byte(info->gpio_num);
+            temp2 = ds18b20_read_byte(info->gpio_num);
+            float temp = 0;
+            temp = (float)(temp1 + (temp2 * 256)) / 16;
+            return temp;
+        }
+        else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
 
-void ds18b20_init(int GPIO){
-  DS_GPIO = GPIO;
-  gpio_pad_select_gpio(DS_GPIO);
-  gpio_set_pull_mode(DS_GPIO, GPIO_PULLUP_ONLY);
-  init=1;
+// Init sensor
+void ds18b20_init(DS18B20_Info_t *info) {
+    char alarm_h = 0, alarm_l = 0;
+    gpio_pad_select_gpio(info->gpio_num);
+    gpio_set_pull_mode(info->gpio_num, GPIO_PULLUP_ONLY);
+    // wait for pin to reach high level
+    ets_delay_us(50);
+    if (ds18b20_RST_PULSE(info->gpio_num) == 1) {
+        info->init = true;
+    } else {
+        info->init = false;
+    }
+
+    ds18b20_send_byte(info->gpio_num, SKIP_ROM);
+    ds18b20_send_byte(info->gpio_num, READ_SCRATCHPAD);
+    ds18b20_read_byte(info->gpio_num);
+    ds18b20_read_byte(info->gpio_num);
+    alarm_h = ds18b20_read_byte(info->gpio_num);
+    alarm_l = ds18b20_read_byte(info->gpio_num);
+
+    ds18b20_RST_PULSE(info->gpio_num);
+
+    ds18b20_send_byte(info->gpio_num, SKIP_ROM);
+    ds18b20_send_byte(info->gpio_num, WRITE_SCRATCHPAD);
+    ds18b20_send_byte(info->gpio_num, alarm_h);
+    ds18b20_send_byte(info->gpio_num, alarm_l);
+    ds18b20_send_byte(info->gpio_num, info->resolution);
 }
--- ds18b20.h
+++ ds18b20.h
@@ -12,15 +12,44 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #ifndef DS18B20_H_
 #define DS18B20_H_
 
-void ds18b20_send(char bit);
-char ds18b20_read();
-void ds18b20_send_byte(char data);
-unsigned char ds18b20_read_byte();
-unsigned char ds18b20_RST_PULSE(void);
-float ds18b20_get_temp(void);
-void ds18b20_init(int GPIO);
+#include "driver/gpio.h"
+
+#define SEARCH_ROM 0XF0
+#define READ_ROM 0x33
+#define MATCH_ROM 0x55
+#define SKIP_ROM 0xCC
+#define ALARM_SEARCH 0xEC
+#define CONVERT_T 0x44
+#define WRITE_SCRATCHPAD 0x4E
+#define READ_SCRATCHPAD 0xBE
+#define COPY_SCRATCHPAD 0x48
+#define RECALL_E2 0xB8
+#define READ_POWER_SUPPLY 0xB4
+
+
+typedef enum {
+    DS18B20_9BIT = 0x1F,
+    DS18B20_10BIT = 0x3F,
+    DS18B20_11BIT = 0x5F,
+    DS18B20_12BIT = 0x7F
+} DS18B20_Resolution_t;
+
+typedef struct {
+    gpio_num_t gpio_num;
+    bool init;
+    DS18B20_Resolution_t resolution;
+} DS18B20_Info_t;
+
+void ds18b20_send(gpio_num_t gpio_num, char bit);
+char ds18b20_read(gpio_num_t gpio_num);
+void ds18b20_send_byte(gpio_num_t gpio_num, char data);
+unsigned char ds18b20_read_byte(gpio_num_t gpio_num);
+unsigned char ds18b20_RST_PULSE(gpio_num_t gpio_num);
+float ds18b20_get_temp(DS18B20_Info_t *info);
+void ds18b20_init(DS18B20_Info_t *info);
 
 #endif
